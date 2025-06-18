4055983dcd170d333dba46f463e4c777b77cb72d
d.kovalenko <dmitry.lipetsk@gmail.com>
2025-06-18 09:15:23 +0300

    [relaxmem] reg_memblock_and_return_ptr is added (refactoring)

--- src/common/relaxmem.c
+++ src/common/relaxmem.c
@@ -29,6 +29,29 @@ static size_t g_mem_block_count__PG = 0;
 
 struct tag_memblock* g_mem_blocks = NULL;
 
+
+static void*
+reg_memblock_and_return_ptr(struct tag_memblock *memblock_ptr, size_t *counter_ptr)
+{
+	Assert(memblock_ptr != NULL);
+	Assert(counter_ptr != NULL);
+	
+	/*
+	* TODO: Use atomic setup (IntelockedCompareExchangePtr)
+	*/
+	memblock_ptr->next = g_mem_blocks;
+
+	g_mem_blocks = memblock_ptr;
+
+	/*
+	* TODO: Use atomic increment (IntelockedIncrement)
+	*/
+	++(*counter_ptr);
+
+	return (void*)(memblock_ptr + 1);
+}
+
+
 void* relaxmem__malloc(size_t sz)
 {
 	size_t const sz2 = sizeof(struct tag_memblock) + sz;
@@ -38,14 +61,10 @@ void* relaxmem__malloc(size_t sz)
 	if (!pv)
 		return NULL;
 
-	((struct tag_memblock*)pv)->next = g_mem_blocks;
 	((struct tag_memblock*)pv)->kind = memkind_STD;
 
-	g_mem_blocks = ((struct tag_memblock*)pv);
-
-	++g_mem_block_count__STD;
-
-	return ((char*)pv) + sizeof(struct tag_memblock);
+	return reg_memblock_and_return_ptr(
+		((struct tag_memblock*)pv), &g_mem_block_count__STD);
 }
 
 void* relaxmem__strdup(const char* str)
@@ -77,14 +96,10 @@ void* relaxmem__pg_malloc(size_t sz)
 	if (!pv)
 		return NULL;
 
-	((struct tag_memblock*)pv)->next = g_mem_blocks;
 	((struct tag_memblock*)pv)->kind = memkind_PG;
 
-	g_mem_blocks = ((struct tag_memblock*)pv);
-
-	++g_mem_block_count__PG;
-
-	return ((char*)pv) + sizeof(struct tag_memblock);
+	return reg_memblock_and_return_ptr(
+		((struct tag_memblock*)pv), &g_mem_block_count__PG);
 }
 
 void* relaxmem__pg_strdup(const char* str)
