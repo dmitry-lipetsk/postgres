58c73940634d59da66308818319bbffe343d7db2
d.kovalenko <dmitry.lipetsk@gmail.com>
2025-06-18 09:24:08 +0300

    relaxmem.h is formatted

--- src/include/common/relaxmem.h
+++ src/include/common/relaxmem.h
@@ -4,14 +4,11 @@
 #include <stddef.h>
 
 extern void* relaxmem__malloc(size_t sz);
-
 extern void* relaxmem__strdup(const char* str);
 
 extern void* relaxmem__pg_malloc(size_t sz);
-
-#define relaxmem__pg_malloc_object(type) ((type *) relaxmem__pg_malloc(sizeof(type)))
-
 extern void* relaxmem__pg_strdup(const char* str);
+#define relaxmem__pg_malloc_object(type) ((type *) relaxmem__pg_malloc(sizeof(type)))
 
 extern void relaxmem__cleanup(void);
 
